1255fd2f98d7b446c1b57456544fce4003c55f2a
Jason Creech <jcreech704@gmail.com>
2021-11-24 12:20:55 -0600

    Corrected transposed file names

--- basics/ex4_array.rb
+++ basics/ex4_array.rb
@@ -1,18 +1,13 @@
-#Exercise 3
+#Exercise 4
 =begin
 Write a program that uses a hash to store a list of movie titles with the year they came out. Then use the puts command to make your program print out the year of each movie to the screen. The output for your program should look something like this.
 
-1975
-2004
-2013
-2001
-1981
-
+Use the dates from the previous example and store them in an array. Then make your program output the same thing as exercise 3.
 =end
 
-movies = {:"Star Wars" => 1977, :"ET" => 1981, :"2001" => 1968, :"Austin Powers" => 1997, :"The Martian" => 2015}
-puts movies[:"Star Wars"]
-puts movies[:"ET"]
-puts movies[:"2001"]
-puts movies[:"Austin Powers"]
-puts movies[:"The Martian"]
+movies = {1977, 1981, 1968, 1997, 2015}
+puts movies[0]
+puts movies[1]
+puts movies[2]
+puts movies[3]
+puts movies[4]
