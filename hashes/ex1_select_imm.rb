b9ab94415c14337f6afe7f147b65e66144de8af7
Jason Creech <jcreech704@gmail.com>
2021-11-30 08:55:03 -0600

    Ex1 nonworking

---
+++ hashes/ex1_select_imm.rb
@@ -0,0 +1,13 @@
+#Exercise 1
+=begin
+Given a hash of family members, with keys as the title and an array of names as the values, 
+use Ruby's built-in select method to gather only immediate family members' 
+names into a new array.
+=end
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+immediate = family.select { |title, names| title == ":sisters" || title == ":brothers" }
+puts immediate.values
