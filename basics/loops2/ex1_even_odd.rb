a07a6bf7f3cc0be6fa52f82f84f93b4ca3d6a444
Jason Creech <jcreech704@gmail.com>
2021-12-04 14:22:58 -0600

    Loops 2 exercises complete

---
+++ basics/loops2/ex1_even_odd.rb
@@ -0,0 +1,12 @@
+#Write a loop that prints numbers 1-5 and whether the number is even or odd. Use the code below to get started.
+count = 1
+
+loop do
+  if count.even?
+    puts "#{count} is even!"
+  else
+    puts "#{count} is odd!"
+  end
+  count += 1
+  break if count > 5
+end
---
+++ basics/loops2/ex2_catch.rb
@@ -0,0 +1,17 @@
+#Modify the following code so that the loop stops if number is equal to or between 0 and 10.
+loop do
+  number = rand(100)
+  puts number
+  break if number >= 0 && number <= 10
+end
+=begin
+another way:
+loop do
+  number = rand(100)
+  puts number
+
+  if number.between?(0, 10)
+    break
+  end
+end
+=end
