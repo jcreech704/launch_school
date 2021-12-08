0b04bcecef8dbbcdee97d1c624e6b5ca32a4b6bc
Jason Creech <jcreech704@gmail.com>
2021-12-08 05:53:53 -0600

    Hashes exercises complete

---
+++ basics/hashes/ex1_car.rb
@@ -0,0 +1,7 @@
+#Create a hash that contains the following data and assign it to a variable named car.
+=begin
+type  color   mileage
+sedan   blue  80_000
+=end
+car = {:type=>"sedan", :color=>"blue", :mileage=>80_000}
+p car
